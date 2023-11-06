600a4d8fce714d9597afe027c2f817d2055a9c5c
John Jazbec <johnny@xhorizont.com>
2023-11-06 13:29:49 +0100

    initial upload

---
+++ apidemo/lib/api_service.dart
@@ -0,0 +1,21 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+import 'constants.dart';
+import 'user_model.dart';
+
+class ApiService {
+  Future<List<UserModel>?> getUsers() async {
+    try {
+      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        List<UserModel> model = userModelFromJson(response.body);
+        return model;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
+}
